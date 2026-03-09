569d1c4dbaa9b99a21ffff174b65abcad577a652
StefanyO8 <stefanysoliveirac@gmail.com>
2026-03-09 20:57:45 -0300

    Splash Screen adicionada e Styles atualizados

---
+++ src/Frontend/App/AlinhaMaisApp/app/src/main/java/com/example/alinhamais/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.alinhamais;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int TIMER_SPLASH = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIMER_SPLASH);
+    }
+}
